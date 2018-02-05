c738d7205b582b89c67728097bdb1d42c662394f
Angus Charles <33369927+feihu1996x@users.noreply.github.com>
2018-02-05 17:38:56 +0800

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -8,14 +8,14 @@
 @version: 1.0
 """
 
-import smtplib,sys,logging
+import smtplib,logging,traceback
 from email.mime.text import MIMEText
 from email.header import Header
 
 logging.basicConfig(level=logging.DEBUG,format="%(asctime)s-%(levelname)s-%(message)s")
 
 class Sendmail:
-	def __init__(self, mailHost, mailUser, mailPassword, sender, receiverList, msgPlain, subject, mailType):
+	def __init__(self, mailHost, mailUser, mailPassword, sender):
 		# smtp服务器
 		self.mailHost = mailHost
 		# 用户名
@@ -24,30 +24,22 @@ class Sendmail:
 		self.mailPassword = mailPassword
 		# 发送方
 		self.sender = sender
-		# 接收方列表
-		self.receiverList = receiverList
+	def sendMail(self, receiverList, msgPlain, subject, mailType):
 		# 邮件正文
-		self.msgPlain = msgPlain
-		# 邮件主题
-		self.subject = subject
-		# 邮件MIME类型
-		self.mailType = mailType
-	def sendMail(self):
-		# 邮件正文
-		message = MIMEText(self.msgPlain, self.mailType, 'utf-8')
+		message = MIMEText(msgPlain, mailType, 'utf-8')
 		message['From'] = Header(self.mailUser)
-		message['To'] = Header(",".join(self.receiverList))
-		message['Subject'] = Header(self.subject, 'utf-8')
+		message['To'] = Header(",".join(receiverList))
+		message['Subject'] = Header(subject, 'utf-8')
 
 		try:
 			smtpObj = smtplib.SMTP()
 			smtpObj.connect(self.mailHost, 25)
 			smtpObj.login(self.mailUser,self.mailPassword)
-			smtpObj.sendmail(self.sender, self.receiverList, message.as_string())
+			smtpObj.sendmail(self.sender, receiverList, message.as_string())
 			logging.debug("邮件发送成功")
 		except smtplib.SMTPException as e:
 			logging.debug(e)
 
 if __name__ == "__main__":
-	# Sendmail(mailHost, mailUser, mailPassword, sender, receiverList, msgPlain, subject, mailType).sendMail()
+	# Sendmail(mailHost, mailUser, mailPassword, sender).sendMail(receiverList, msgPlain, subject, mailType)
 
